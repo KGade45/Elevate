8a8510a1c88e51f5aebbef43bb6b3d74b8ebed33
KGade45 <koustubhgade@gmail.com>
2025-08-05 12:11:22 +0530

    Initial commit

---
+++ Elevate/ElevateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElevateApp.swift
+//  Elevate
+//
+//  Created by Kaustubh Gade on 05/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ElevateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
