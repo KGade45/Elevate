08c8c0fbe085e264aee4c28ecc570f7bf01f0a90
Kaustubh Gade <koustubhgade@gmail.com>
2025-08-26 18:55:12 +0530

    minor changes

---
+++ Elevate/Core/Services/AppwriteClient.swift
@@ -0,0 +1,8 @@
+//
+//  AppwriteClient.swift
+//  Elevate
+//
+//  Created by Kaustubh kailas gade on 26/08/25.
+//
+
+import Foundation
---
+++ Elevate/Core/Services/PostsService.swift
@@ -0,0 +1,8 @@
+//
+//  PostsService.swift
+//  Elevate
+//
+//  Created by Kaustubh kailas gade on 26/08/25.
+//
+
+import Foundation
